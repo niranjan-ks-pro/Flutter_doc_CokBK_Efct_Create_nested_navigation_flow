fd37fb7f1e820903222f07ab26e35ba85fda69a8
Niranjan-KS <niranjan.ks.pro@gmail.com>
2023-07-25 16:51:40 +0530

    commit

---
+++ create_a_nested_navigation_flow/README.md
@@ -0,0 +1,3 @@
+# create_a_nested_navigation_flow
+
+A new Flutter project.
---
+++ create_a_nested_navigation_flow/android/app/src/main/kotlin/com/example/create_a_nested_navigation_flow/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.create_a_nested_navigation_flow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
